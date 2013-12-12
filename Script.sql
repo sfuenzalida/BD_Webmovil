644ebdf7a0e572879394ac34f3a55b30eb527100
sfuenzalida <s.fuenzalida17@gmail.com>
2013-12-11 22:20:18 -0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -11,109 +11,73 @@ CREATE DATABASE "DBWebMovil"
        CONNECTION LIMIT = -1;
 
 -- Table: usuarios
-
 -- DROP TABLE usuarios;
-
 -- Dependiendo privilegios se ve lo que pude hacer cada usuario
 
-
-
 CREATE TABLE usuarios
-
 (
-  
 	id_user text NOT NULL,
-
   	pass text,
- 
 	privilegios numeric,
-
   	CONSTRAINT usuarios_pkey PRIMARY KEY (id_user)
-
 )
-
-
 WITH (
-  
-OIDS=FALSE
-
+	OIDS=FALSE
 );
-
-
 ALTER TABLE usuarios
-  
 	OWNER TO postgres;
 
 -- Table: "Servicios_user"
-
 -- DROP TABLE "Servicios_user";
-
 -- tabla para requerir servicios, de la pagina.
 
 CREATE TABLE "Servicios_user"
 (
-  rut "char"[] NOT NULL,
-  tipo_user text,
-  email text,
-  requerimientos text,
-  CONSTRAINT "Servicios_user_pkey" PRIMARY KEY (rut)
+	rut "char"[] NOT NULL,
+	tipo_user text,
+	email text,
+	requerimientos text,
+	CONSTRAINT "Servicios_user_pkey" PRIMARY KEY (rut)
 )
 WITH (
-  OIDS=FALSE
+  	OIDS=FALSE
 );
 ALTER TABLE "Servicios_user"
-  OWNER TO postgres;
+  	OWNER TO postgres;
   
-  
-  -- Table: publicaciones 
-  -- DROP TABLE publicaciones; 
-  -- Tabla con pequeñas palabras de publicaciones, para hacer mas rapida la busqueda.
-  CREATE TABLE publicaciones 
-  ( 
-     id_public numeric[] NOT NULL,
-     tipo text[],
-     fecha text[],
-     CONSTRAINT publicaciones_pkey PRIMARY KEY (id_public)
-  )
-  WITH ( 
-     OIDS=FALSE 
-  ); 
-  ALTER TABLE publicaciones 
-  OWNER TO postgres;
+-- Table: publicaciones 
+-- DROP TABLE publicaciones; 
+-- Tabla con pequeñas palabras de publicaciones, para hacer mas rapida la busqueda.
+
+CREATE TABLE publicaciones 
+( 
+     	id_public numeric[] NOT NULL,
+     	tipo text[],
+     	fecha text[],
+     	CONSTRAINT publicaciones_pkey PRIMARY KEY (id_public)
+)
+WITH ( 
+	OIDS=FALSE 
+); 
+ALTER TABLE publicaciones 
+  	OWNER TO postgres;
+ 
   
 -- Table: ofer_trabajo
-
-
 -- DROP TABLE ofer_trabajo;
-
-
+-- Tabla para bucar ofertas, teniendo fecha, contacto o empresa
 
 CREATE TABLE ofer_trabajo
-
 (
-  
-id_job numeric[] NOT NULL,
- 
-"Tipo_job" text[],
-  
-"Contacto" text[],
-  
-"Empresa" text[],
-  
-fecha text[],
-  
-CONSTRAINT ofer_trabajo_pkey PRIMARY KEY (id_job)
-
+  	id_job numeric[] NOT NULL,
+	"Tipo_job" text[],
+	"Contacto" text[],
+	"Empresa" text[],
+	fecha text[],
+	CONSTRAINT ofer_trabajo_pkey PRIMARY KEY (id_job)
 )
-
-
 WITH (
-  
-OIDS=FALSE
-
+	OIDS=FALSE
 );
-
-
 ALTER TABLE ofer_trabajo
-  
-OWNER TO postgres;
+  	OWNER TO postgres;
